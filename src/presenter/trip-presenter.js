5b1c46f02c0e67cef3cf9aefd20dd5dbb5f776dd
GotLib567 <gotlibweb@mail.ru>
2023-03-25 15:45:12 +0500

    2503

---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,22 @@
+import EventsListView from '../view/events-list.js';
+import CreationFormView from '../view/creation-form-view';
+import EditorFormView from '../view/editor-form-view';
+import WaypointListView from '../view/waypoint-list-view';
+import { render } from '../render';
+
+export default class TripPresenter {
+  constructor() {
+    this.tripEvents = document.querySelector('.trip-events');
+    this.eventsList = new EventsListView();
+  }
+
+  init() {
+    render(this.eventsList, this.tripEvents);
+    render(new CreationFormView(), this.eventsList.getElement());
+    render(new EditorFormView(), this.eventsList.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new WaypointListView(), this.eventsList.getElement());
+    }
+  }
+}
